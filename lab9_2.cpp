f650493cae132d121f6bb3671cb8c2facd8b573e
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2021-12-07 16:56:24 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,6 +3,23 @@
 #include<string>
 using namespace std;
 
+char findGrade(double x){
+	char y;
+	if ( x > 90)
+	y = 'A';
+	else if ( x > 75)
+	y = 'B';
+	else if ( x > 60)
+	y = 'C';
+	else if ( x > 45)
+	y = 'D';
+	else 
+	y = 'F';
+
+	return y;
+
+}
+
 //[Missing Code 1] Write definition of the function findGrade() here.
 
 int main(){
@@ -17,9 +34,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
